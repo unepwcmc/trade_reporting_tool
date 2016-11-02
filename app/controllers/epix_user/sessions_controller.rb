d0763e9391d9f60439a5b376f7a3d67f97611cbc
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-11-02 14:06:14 +0000

    Autofocus based on email presence

--- app/controllers/epix_user/sessions_controller.rb
+++ app/controllers/epix_user/sessions_controller.rb
@@ -1,6 +1,8 @@
 class EpixUser::SessionsController < Devise::SessionsController
   def new
     @email = params[:user]
+    @autofocus_email = !@email.present?
+    @autofocus_password = !@autofocus_email
     super
   end
 end
