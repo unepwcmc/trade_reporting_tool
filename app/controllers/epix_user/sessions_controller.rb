b81eca44b008370c43403dac206ef1f41779d7f1
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-11-03 13:12:50 +0000

    Use just one variable for autofocus

--- app/controllers/epix_user/sessions_controller.rb
+++ app/controllers/epix_user/sessions_controller.rb
@@ -2,7 +2,6 @@ class EpixUser::SessionsController < Devise::SessionsController
   def new
     @email = params[:user]
     @autofocus_email = !@email.present?
-    @autofocus_password = !@autofocus_email
     super
   end
 end
--- app/controllers/sapi_user/sessions_controller.rb
+++ app/controllers/sapi_user/sessions_controller.rb
@@ -2,7 +2,6 @@ class SapiUser::SessionsController < ::Devise::SessionsController
   def new
     @email = params[:user]
     @autofocus_email = !@email.present?
-    @autofocus_password = !@autofocus_email
     super
   end
 end
