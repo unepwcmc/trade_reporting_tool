1fe64fd7435dffb87bfe64cd82b72fcaf35a18e4
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-11-04 15:23:13 +0000

    Add specs for filter by submitted and in progress uploads

--- spec/controllers/annual_report_uploads_controller_spec.rb
+++ spec/controllers/annual_report_uploads_controller_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe AnnualReportUploadsController, type: :controller do
       @epix_user = FactoryGirl.create(:epix_user)
       @sapi_user = FactoryGirl.create(:sapi_user)
       @epix_upload = FactoryGirl.create(:annual_report_upload, epix_created_by_id: @epix_user.id)
-      2.times { FactoryGirl.create(:annual_report_upload, created_by_id: @sapi_user.id) }
+      2.times {
+        FactoryGirl.create(
+          :annual_report_upload,
+          created_by_id: @sapi_user.id,
+          submitted_by_id: @sapi_user.id
+        )
+      }
     end
 
     context "when logging in from epix" do
@@ -26,6 +32,24 @@ RSpec.describe AnnualReportUploadsController, type: :controller do
         get :index
 
         expect(assigns(:annual_report_uploads).size).to eq(3)
+      end
+    end
+
+    context "filter by submitted and in progress" do
+      it "should assign only submitted uploads" do
+        @request.env['devise.mapping'] = Devise.mappings[:sapi_user]
+        sign_in @sapi_user
+        get :index
+
+        expect(assigns(:submitted_uploads).size).to eq(2)
+      end
+
+      it "should assign only in progress uploads" do
+        @request.env['devise.mapping'] = Devise.mappings[:sapi_user]
+        sign_in @sapi_user
+        get :index
+
+        expect(assigns(:in_progress_uploads).size).to eq(1)
       end
     end
   end
