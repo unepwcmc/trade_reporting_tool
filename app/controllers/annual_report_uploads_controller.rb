2054bd4e10bb7bc01b375b2271c7b587ba7bd20d
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-11-04 11:08:13 +0000

    Filter uploads by epix user or show all if from sapi

--- app/controllers/annual_report_uploads_controller.rb
+++ app/controllers/annual_report_uploads_controller.rb
@@ -2,8 +2,9 @@ class AnnualReportUploadsController < ApplicationController
   respond_to :json
 
   def index
+    epix_user_id = current_epix_user && current_epix_user.id
     @annual_report_uploads =
-      Sapi::Trade::AnnualReportUpload.all.map do |aru|
+      Sapi::Trade::AnnualReportUpload.created_by(epix_user_id).map do |aru|
         AnnualReportUploadSerializer.new(aru)
       end
   end
--- app/models/sapi/trade/annual_report_upload.rb
+++ app/models/sapi/trade/annual_report_upload.rb
@@ -1,5 +1,7 @@
 module Sapi
   class Trade::AnnualReportUpload < Sapi::Base
     self.table_name = 'trade_annual_report_uploads'
+
+    scope :created_by, -> (user_id) { where({created_by_id: user_id, is_from_epix: true}) if user_id }
   end
 end
