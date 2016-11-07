2a4780d109d9b2d8ef271b7d50d05b284461c006
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-11-07 12:52:47 +0000

    Fix and add specs

--- spec/controllers/epix_user/sessions_controller_spec.rb
+++ spec/controllers/epix_user/sessions_controller_spec.rb
@@ -4,14 +4,20 @@ RSpec.describe EpixUser::SessionsController, type: :controller do
   describe "GET new" do
     before(:each) do
       @user = FactoryGirl.create(:epix_user)
+      @request.env['devise.mapping'] = Devise.mappings[:epix_user]
+      @request.env['HTTP_REFERER'] = 'http://test.host/epix/users/sign_in?user="email"'
+      sign_in @user
     end
     it "should use Epix::User as resource to login" do
-      @request.env['devise.mapping'] = Devise.mappings[:epix_user]
-      sign_in @user
       get :new
 
-      expect(response.status).to eq(302)
       expect(subject.current_epix_user.class).to eq(Epix::User)
+    end
+
+    it "should redirect to annual report uploads index page" do
+      get :new
+
+      expect(response).to redirect_to(annual_report_uploads_path)
     end
   end
 end
