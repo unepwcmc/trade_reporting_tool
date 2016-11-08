eef98d1cb097fb1c0ce19ebb1f28220e089acf51
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-11-08 16:34:38 +0000

    Config application.rb to include shared stylesheets

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,9 @@ module TradeReportingTool
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_record.schema_format = :sql
+
+    # Load shared assets from epix_frontend
+    config.assets.paths << "#{Rails.root}/app/assets/shared/stylesheets"
+    config.assets.paths << "#{Rails.root}/app/assets/shared/fonts"
   end
 end
