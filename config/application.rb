223018c4ebd4e5de9abd76f2a9e5ecedc33408ce
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-11-09 10:48:35 +0000

    Add shared javascripts path

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module TradeReportingTool
 
     # Load shared assets from epix_frontend
     config.assets.paths << "#{Rails.root}/app/assets/shared/stylesheets"
+    config.assets.paths << "#{Rails.root}/app/assets/shared/javascripts"
     config.assets.paths << "#{Rails.root}/app/assets/shared/fonts"
   end
 end
