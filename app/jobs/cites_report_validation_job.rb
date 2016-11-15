a656156caa9dc6c156fa6f17a1dc5a915f8f76e3
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-11-15 11:49:02 +0000

    run validation rules in worker

--- app/jobs/cites_report_validation_job.rb
+++ app/jobs/cites_report_validation_job.rb
@@ -10,7 +10,21 @@ class CitesReportValidationJob < ApplicationJob
       Rails.logger.warn "CITES Report not found #{aru_id}"
       return
     end
-    # TODO:
+    result = {}
+    aru.process_validation_rules
+    if !aru.primary_validation_errors.empty?
+      result[:Status] = 'ERROR'
+      result[:Message] = 'Primary errors detected in CITES Report'
+      # TODO: here we go with errors row by row
+    elsif !aru.secondary_validation_errors.empty? && !force_submit
+      result[:Status] = 'ERROR'
+      result[:Message] = 'Secondary errors detected in CITES Report'
+      # TODO: here we go with errors row by row
+    else
+      result[:Status] = 'SUCCESS'
+      # TODO: proceed with submission
+    end
+    Rails.logger.info result
     Rails.logger.info "Finished validation of CITES Report #{aru_id}"
   end
 end
