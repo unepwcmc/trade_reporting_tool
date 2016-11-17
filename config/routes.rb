6564b6634c12ae3ffff5f04529ae08a00b5a14d1
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-11-17 12:30:52 +0000

    Redirect root path to annual_report_uploads

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,5 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: redirect('/annual_report_uploads')
 end
