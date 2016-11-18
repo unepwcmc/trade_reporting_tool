f7247a8f22986969c182882d8f61ac383dabbf15
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-11-18 10:16:26 +0000

    job spec

--- spec/jobs/cites_report_validation_job_spec.rb
+++ spec/jobs/cites_report_validation_job_spec.rb
@@ -1,9 +1,22 @@
 require "rails_helper"
 
-RSpec.describe CitesReportValidationJob do
-  it "matches with enqueued job" do
-    ActiveJob::Base.queue_adapter = :test
-    CitesReportValidationJob.perform_later
-    expect(CitesReportValidationJob).to have_been_enqueued
+RSpec.describe CitesReportValidationJob, type: :job do
+  include ActiveJob::TestHelper
+
+  let(:aru){ FactoryGirl.create(:annual_report_upload) }
+  subject(:job) { described_class.perform_later(aru.id, false) }
+
+  it 'queues the job' do
+    expect { job }
+      .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'is in default queue' do
+    expect(CitesReportValidationJob.new.queue_name).to eq('default')
+  end
+
+  it 'executes perform' do
+    expect(CitesReportValidator).to receive(:call).with(aru.id, false)
+    perform_enqueued_jobs { job }
   end
 end
