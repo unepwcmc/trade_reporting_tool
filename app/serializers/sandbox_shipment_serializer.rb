4b4b5e48beaedde3891bfddd8761a0ed09031848
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-11-29 14:48:37 +0000

    Add versions and changes fields in serializer

--- app/serializers/sandbox_shipment_serializer.rb
+++ app/serializers/sandbox_shipment_serializer.rb
@@ -2,7 +2,7 @@ class SandboxShipmentSerializer < ActiveModel::Serializer
   attributes :id, :appendix, :taxon_name, #:reported_taxon_name, :accepted_taxon_name,
     :term_code, :quantity, :unit_code, :trading_partner, :country_of_origin,
     :export_permit, :origin_permit, :purpose_code, :source_code,
-    :year, :import_permit
+    :year, :import_permit, :versions, :changes
 
 #  def reported_taxon_name
 #    object.reported_taxon_concept && "#{object.reported_taxon_concept.full_name} (#{object.reported_taxon_concept.name_status})" ||
@@ -12,6 +12,14 @@ class SandboxShipmentSerializer < ActiveModel::Serializer
 #  def accepted_taxon_name
 #    object.taxon_concept && "#{object.taxon_concept.full_name} (#{object.taxon_concept.name_status})"
 #  end
+  #
+  def versions
+    object.versions.map(&:reify)
+  end
+
+  def changes
+    object.versions.map(&:changeset)
+  end
 
 end
 
