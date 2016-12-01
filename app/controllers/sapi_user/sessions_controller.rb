5c11eacde840820ed8e1aa877ff22da71f70b9db
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-12-01 11:16:57 +0000

    Check authorisation for sapi user

--- app/controllers/sapi_user/sessions_controller.rb
+++ app/controllers/sapi_user/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SapiUser::SessionsController < ::Devise::SessionsController
+  before_action :is_authorised?, only: [:create]
+
   def new
     @email = params[:user]
     @autofocus_email = !@email.present?
     super
+  end
+
+  def is_authorised?
+    email = params[:sapi_user][:email]
+    user = Sapi::User.find_by_email(email)
+    if user && user.role != 'admin'
+      redirect_to new_sapi_user_session_path, flash: { alert: t('unauthorised') }
+    end
   end
 end
