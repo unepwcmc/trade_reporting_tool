f863e34c08a63dfa6321a60ae79e4df9c5ada9be
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-12-01 11:17:52 +0000

    Add specs for sapi user login

--- spec/controllers/sapi_user/sessions_controller_spec.rb
+++ spec/controllers/sapi_user/sessions_controller_spec.rb
@@ -20,4 +20,32 @@ RSpec.describe SapiUser::SessionsController, type: :controller do
       expect(response).to redirect_to(annual_report_uploads_path)
     end
   end
+
+  describe "POST create" do
+    context "when user is not an admin" do
+      before(:each) do
+        @user = FactoryGirl.create(:sapi_user, role: '')
+        @request.env['devise.mapping'] = Devise.mappings[:sapi_user]
+        @request.env['HTTP_REFERER'] = 'http://test.host/sapi/users/sign_in?user="email"'
+      end
+      it "should redirect to login because unauthorised" do
+        post :create, sapi_user: {email: @user.email }
+
+        expect(response).to redirect_to(new_sapi_user_session_path)
+      end
+    end
+    context "when user is admin" do
+      before(:each) do
+        @user = FactoryGirl.create(:sapi_user)
+        @request.env['devise.mapping'] = Devise.mappings[:sapi_user]
+        @request.env['HTTP_REFERER'] = 'http://test.host/sapi/users/sign_in?user="email"'
+      end
+      it "should login correctly" do
+        post :create, sapi_user: {email: @user.email }
+
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+
 end
