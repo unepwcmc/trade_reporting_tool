b66bd1d135c74870d6ccba6d400441c34244e7d8
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-12-02 12:56:37 +0000

    Extract destroy authorisation logic in filter

--- app/controllers/annual_report_uploads_controller.rb
+++ app/controllers/annual_report_uploads_controller.rb
@@ -1,5 +1,6 @@
 class AnnualReportUploadsController < ApplicationController
   before_action :authenticate_user!
+  before_action :authorise_destroy, only: [:destroy]
   respond_to :json
 
   def index
@@ -44,16 +45,11 @@ class AnnualReportUploadsController < ApplicationController
 
   def destroy
     @annual_report_upload = Trade::AnnualReportUpload.find(params[:id])
-    deleted = true
-    unless @annual_report_upload.user_authorised_to_destroy?(current_user)
-      deleted = false
+    if @annual_report_upload.destroy_with_sandbox
+      flash[:notice] = t('aru_deleted')
     else
-      unless @annual_report_upload.submitted_at.present?
-        deleted = @annual_report_upload.sandbox.try(:destroy) &&
-          @annual_report_upload.destroy
-      end
+      flash[:alert] = t('aru_not_deleted')
     end
-    deleted ? flash[:notice] = t('aru_deleted') : flash[:alert] = t('aru_not_deleted')
     redirect_to annual_report_uploads_path
   end
 
@@ -61,6 +57,26 @@ class AnnualReportUploadsController < ApplicationController
 
   def authenticate_user!
     render "unauthorised" unless (current_epix_user || current_sapi_user).present?
+  end
+
+  def authorise_destroy
+    aru = Trade::AnnualReportUpload.find(params[:id])
+    creator = aru.creator
+    authorised = true
+    if creator.is_a?(Epix::User)
+      if current_user.is_a?(Sapi::User)
+        flash[:alert] = t('aru_not_deleted')
+        redirect_to annual_report_uploads_path and return true
+      end
+      authorised = (current_user.id == creator.id ||
+        (current_user.organisation.id == creator.organisation.id && current_user.is_admin))
+    else
+      authorised = current_user.is_a?(Sapi::User) && current_user.role == Sapi::User::MANAGER
+    end
+    if !authorised || aru.submitted_at.present?
+      flash[:alert] = t('aru_not_deleted')
+      redirect_to annual_report_uploads_path
+    end
   end
 
 end
