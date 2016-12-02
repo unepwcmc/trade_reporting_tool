7c8c59a02c591300abcbd0d275eeaa0bc19aa985
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-12-02 15:32:13 +0000

    Fix specs

--- spec/controllers/shipments_controller_spec.rb
+++ spec/controllers/shipments_controller_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe ShipmentsController, type: :controller do
   describe "GET index" do
     before(:each) do
       @epix_user = FactoryGirl.create(:epix_user)
+      @aru = FactoryGirl.create(:annual_report_upload)
       @shipment = FactoryGirl.create(:sandbox_template)
     end
 
     it "should assigns total number of pages" do
       @request.env['devise.mapping'] = Devise.mappings[:epix_user]
       sign_in @epix_user
-      get :index
+      get :index, annual_report_upload_id: @aru
 
       expect(assigns(:total_pages)).to eq(1)
     end
