9ec814ee39c84fabe369dee92ac05bd5f3c6063c
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-12-15 13:31:42 +0000

    Add specs for changes_history_pdf

--- spec/controllers/api/v1/annual_report_uploads_controller_spec.rb
+++ spec/controllers/api/v1/annual_report_uploads_controller_spec.rb
@@ -105,4 +105,54 @@ RSpec.describe Api::V1::AnnualReportUploadsController, type: :controller do
       end
     end
   end
+  describe "GET changes_history_pdf" do
+    before(:each) do
+      @epix_user = FactoryGirl.create(:epix_user)
+      @sapi_user = FactoryGirl.create(:sapi_user)
+      @epix_upload = FactoryGirl.create(:annual_report_upload, epix_creator: @epix_user)
+      2.times {
+        FactoryGirl.create(
+          :annual_report_upload,
+          created_by_id: @sapi_user.id,
+          submitted_by_id: @sapi_user.id,
+          submitted_at: DateTime.now
+        )
+      }
+      sandbox = @epix_upload.sandbox
+      sandbox.copy_data({
+        CITESReport: [
+          {
+            CITESReportRow:  {
+              TradingPartnerId:  "FR",
+              Year:  2016,
+              ScientificName:  "Alligator mississipiensis",
+              Appendix:  nil,
+              TermCode:  "SKI",
+              Quantity:  5.0,
+              UnitCode:  "KIL",
+              SourceCode:  "W",
+              PurposeCode:  "Z",
+              OriginCountryId:  "US",
+              OriginPermitId:  nil,
+              ExportPermitId:  "CH123",
+              ImportPermitId:  nil
+            }
+          }
+        ]
+      })
+      @sandbox_template = sandbox.ar_klass.first
+      @sandbox_template.update_attributes(year: 2015)
+    end
+
+    context "Retrieve shipments" do
+      it "should get shipments with changes only", versioning: true do
+        @request.env['devise.mapping'] = Devise.mappings[:sapi_user]
+        sign_in @sapi_user
+
+        get :changes_history_pdf, id: @epix_upload.id
+
+        expect(assigns(:shipments).size).to eq(1)
+      end
+    end
+  end
 end
