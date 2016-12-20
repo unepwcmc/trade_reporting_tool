c1dc5e20802a311b010c2cf4a0f1696a97bf9034
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-12-20 14:42:05 +0000

    fixed formatting

--- spec/support/validation_rules_helpers.rb
+++ spec/support/validation_rules_helpers.rb
@@ -34,14 +34,16 @@ shared_context :validation_rules_helpers do
   end
 
   def create_taxon_concept_appendix_year_validation
-    FactoryGirl.create(:taxon_concept_appendix_year_validation_rule,
+    FactoryGirl.create(
+      :taxon_concept_appendix_year_validation_rule,
       :is_primary => false,
       :is_strict => true
     )
   end
 
   def create_term_unit_validation
-    FactoryGirl.create(:inclusion_validation_rule,
+    FactoryGirl.create(
+      :inclusion_validation_rule,
       :column_names => ['term_code', 'unit_code'],
       :valid_values_view => 'valid_term_unit_view',
       :is_primary => false
@@ -49,7 +51,8 @@ shared_context :validation_rules_helpers do
   end
 
   def create_term_purpose_validation
-    FactoryGirl.create(:inclusion_validation_rule,
+    FactoryGirl.create(
+      :inclusion_validation_rule,
       :column_names => ['term_code', 'purpose_code'],
       :valid_values_view => 'valid_term_purpose_view',
       :is_primary => false
@@ -57,7 +60,8 @@ shared_context :validation_rules_helpers do
   end
 
   def create_taxon_concept_term_validation
-    FactoryGirl.create(:inclusion_validation_rule,
+    FactoryGirl.create(
+      :inclusion_validation_rule,
       :column_names => ['taxon_concept_id', 'term_code'],
       :valid_values_view => 'valid_taxon_concept_term_view',
       :is_primary => false,
@@ -66,7 +70,8 @@ shared_context :validation_rules_helpers do
   end
 
   def create_taxon_concept_country_of_origin_validation
-    FactoryGirl.create(:inclusion_validation_rule,
+    FactoryGirl.create(
+      :inclusion_validation_rule,
       :scope => {
         :rank => { :inclusion => [Rank::SPECIES, Rank::SUBSPECIES] },
         :source_code => { :inclusion => ['W'] },
@@ -80,7 +85,8 @@ shared_context :validation_rules_helpers do
   end
 
   def create_taxon_concept_exporter_validation
-    FactoryGirl.create(:inclusion_validation_rule,
+    FactoryGirl.create(
+      :inclusion_validation_rule,
       :scope => {
         :rank => { :inclusion => [Rank::SPECIES, Rank::SUBSPECIES] },
         :source_code => { :inclusion => ['W'] },
@@ -95,7 +101,8 @@ shared_context :validation_rules_helpers do
   end
 
   def create_exporter_country_of_origin_validation
-    FactoryGirl.create(:distinct_values_validation_rule,
+    FactoryGirl.create(
+      :distinct_values_validation_rule,
       :column_names => ['exporter', 'country_of_origin'],
       :is_primary => false,
       :is_strict => true
@@ -103,7 +110,8 @@ shared_context :validation_rules_helpers do
   end
 
   def create_exporter_importer_validation
-    FactoryGirl.create(:distinct_values_validation_rule,
+    FactoryGirl.create(
+      :distinct_values_validation_rule,
       :column_names => ['exporter', 'importer'],
       :is_primary => false,
       :is_strict => true
@@ -111,7 +119,8 @@ shared_context :validation_rules_helpers do
   end
 
   def create_taxon_concept_source_validation
-    FactoryGirl.create(:taxon_concept_source_validation_rule,
+    FactoryGirl.create(
+      :taxon_concept_source_validation_rule,
       :column_names => ['taxon_concept_id', 'source_code'],
       :is_primary => false,
       :is_strict => true
