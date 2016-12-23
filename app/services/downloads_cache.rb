24b7b5650ad9c9446f68316883b17b1e2ee3ae48
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-12-23 00:41:44 +0000

    Use httparty to invoke cache cleaning on SAPI

--- app/services/downloads_cache.rb
+++ app/services/downloads_cache.rb
@@ -1,23 +1,5 @@
 module DownloadsCache
-
-  def self.downloads_path(dir)
-    "#{Rails.root}/public/downloads/#{dir}"
-  end
-
-  def self.clear_dirs(dirs)
-    dirs.each do |dir|
-      Rails.logger.debug("Clearing #{dir}")
-      FileUtils.rm_rf(Dir["#{downloads_path(dir)}/*"], :secure => true)
-    end
-  end
-
-  # cleared after save & destroy
   def self.clear_shipments
-    clear_dirs(['shipments'])
-    clear_dirs(['comptab'])
-    clear_dirs(['gross_exports'])
-    clear_dirs(['gross_imports'])
-    clear_dirs(['net_exports'])
-    clear_dirs(['net_imports'])
+    response = HTTParty.get("#{Rails.application.secrets.sapi_path}/api/trade_downloads_cache_cleanup")
   end
 end
