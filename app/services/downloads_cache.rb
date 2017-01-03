282b571750ea3f23cbb0da063858afd165264f9f
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-01-03 08:42:23 +0000

    synced secrets & env.example

--- app/services/downloads_cache.rb
+++ app/services/downloads_cache.rb
@@ -1,7 +1,7 @@
 module DownloadsCache
   def self.clear_shipments
     Rails.logger.info(
-      HTTParty.get("#{Rails.application.secrets.sapi_path}/api/trade_downloads_cache_cleanup")
+      HTTParty.get("#{Rails.application.secrets.sapi_url}/api/trade_downloads_cache_cleanup")
     )
   end
 end
