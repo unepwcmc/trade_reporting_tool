7b94b4a33cf9f5b448f1dfd715f1fc0323d4e7e9
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-01-17 11:54:28 +0000

    Changelog generator to accept duplicates object

--- app/services/changelog_csv_generator.rb
+++ app/services/changelog_csv_generator.rb
@@ -1,7 +1,8 @@
 require 'csv'
 class ChangelogCsvGenerator
+  DEFAULT_COLUMNS = ['ID', 'Version', 'OP', 'ChangedAt', 'ChangedBy']
 
-  def self.call(aru, requester, aws=false)
+  def self.call(aru, requester, aws=false, duplicates=nil)
     data_columns = if aru.reported_by_exporter?
       Trade::SandboxTemplate::EXPORTER_COLUMNS
     else
@@ -26,20 +27,25 @@ class ChangelogCsvGenerator
 
     tempfile = Tempfile.new(["changelog_#{requester_type}_#{aru.id}-", ".csv"], Rails.root.join('tmp'))
 
-    ar_klass = aru.sandbox(true).ar_klass
+    ar_klass = aru.sandbox.ar_klass
+
+    all_columns = DEFAULT_COLUMNS + data_columns.map(&:camelize)
+    all_columns = all_columns + ['Duplicate'] if duplicates
 
     CSV.open(tempfile, 'w', headers: true) do |csv|
-      csv << ['ID', 'Version', 'OP', 'ChangedAt', 'ChangedBy'] +
-        data_columns.map(&:camelize)
+      csv << all_columns
       limit = 100
       offset = 0
       query = ar_klass.includes(:versions).limit(limit).offset(offset)
       while query.any?
         query.all.each do |shipment|
-          csv << [shipment.id, nil, nil, shipment.created_at, nil] +
+          duplicate = (duplicates && duplicates.split(',').include?(shipment.id.to_s)) ? 'D' : ''
+          values = [shipment.id, nil, nil, shipment.created_at, nil] +
             data_columns.map do |dc|
               shipment[dc]
             end
+          values = values + [duplicate] if duplicates
+          csv << values
 
           shipment.versions.each do |version|
             reified = version.reify(dup: true)
@@ -54,12 +60,15 @@ class ChangelogCsvGenerator
                 sapi_users && sapi_users[id_as_number] || 'WCMC'
               end
             end
-            csv << [
+            values = [
                 version.item_id, version.id, version.event, version.created_at, whodunnit
               ] +
               data_columns.map do |dc|
                 reified[dc]
               end
+            values = values + [''] if duplicates
+
+            csv << values
           end
 
           offset += limit
